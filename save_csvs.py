0b588127cf6b3ae287f30f658fddc31a49d577a2
Andreas Calleja <andreascalleja@gmail.com>
2022-11-09 21:21:39 +0100

    Added bundesliga csvs

--- save_csvs.py
+++ save_csvs.py
@@ -3,13 +3,10 @@ import time
 
 def main():
     base_url = 'https://fbref.com/en/comps/13/{}-{}/{}-{}-Bundesliga-Stats'
-    for year in range(1995, 2022):
-        print(year)
+    for year in range(1994, 1995):
         url = base_url.format(year, year+1, year, year+1)
         html = pd.read_html(url, header=0)
         df = html[0]
         df.to_csv(f'csvs/bundesliga/bundesliga_{year}-{year+1}.csv', index=False)
-        print('sleeping')
-        time.sleep(5)
 
 main()
